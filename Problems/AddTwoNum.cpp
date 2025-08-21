9d5737d898d16c0aa7068388f1e1dda77ad4d5f2
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-08-21 12:44:30 +0530

    Add Q.Add Two Numbers

---
+++ Problems/AddTwoNum.cpp
@@ -0,0 +1,10 @@
+// Problem : Add Two Numbers
+// Source : https://www.geeksforgeeks.org/problems/add-two-numbers-1628835901/1?page=2&sortBy=accuracy
+
+class Solution {
+  public:
+    int sum(int x, int y) {
+        //your code goes here
+        return x+y;
+    }
+};
